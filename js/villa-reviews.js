ddd51c8a001cd7c924bea9802d1613fe4bbcac02
ASMAJIC784 <amelsmajic@gmail.com>
2025-05-27 04:15:22 +0200

    review update

--- js/villa-reviews.js
+++ js/villa-reviews.js
@@ -28,10 +28,63 @@ const villaReviews = {
     }
   ],
   laura: [
-    // Add Laura reviews here
+    {
+      reviewer: "Эдита",
+      stars: 5,
+      text: "A wonderful villa, fully equipped with everything you need. Modern renovation, each room has its own bathroom, large pool, beautiful view of the vineyards. The owner is very caring and attentive. Highly recommend, will definitely come back!"
+    },
+    {
+      reviewer: "Katarina",
+      stars: 5,
+      text: "Das Haus ist toll, sauber und die Chefin super nett und zuvorkommend. Es war sehr sauber und wie auf den Bildern. Es gab leider keine Fliegengitter an den Fenstern."
+    },
+    {
+      reviewer: "Jasmine",
+      stars: 4,
+      text: "Wunderschönes Ferienhaus in idyllischer ruhiger Lage. Der Außenbereich mit Außenküche war das Highlight. Jedes Schlafzimmer hat sein eigenes Bad und TV mit Netflix. Sehr ruhig, Auto notwendig. Vermieterin sehr nett und hilfsbereit. Sehr empfehlenswert!"
+    },
+    {
+      reviewer: "Tihomir",
+      stars: 5,
+      text: "Wer Ruhe vom Alltag sucht, ist hier gut aufgehoben. Der große Pool war für die Kids ein Highlight und jedes Zimmer hat sein eigenes Badezimmer. Die Besitzerin ist immer für einen da und löst Probleme schnell und freundlich. Kommen gerne wieder."
+    },
+    {
+      reviewer: "Boris",
+      stars: 5,
+      text: "Sve pohvale, vratit ćemo se sigurno! Domaćini su učinili sve da se osjećamo ugodno i dobrodošlo. Vila je lijepo uređena, vanjska kućica sa roštiljem pored bazena odlična, djeca su uživala u bazenu i vrtu."
+    },
+    {
+      reviewer: "Barbara (Germany)",
+      stars: 4,
+      text: "Ein tolles Haus, sehr zu empfehlen. Sehr geräumig mit schöner Einrichtung. Besonders gefallen hat uns die große Outdoor Küche und der Pool. Die Vermieterin war sehr hilfsbereit. Uns hat alles gefallen."
+    },
+    {
+      reviewer: "Barbara (Croatia)",
+      stars: 5,
+      text: "Vila Laura je savršena za odmor! Mirna lokacija, puno zelenila, kuća prostrana, nova, čista, potpuno opremljena. Uživali smo u bazenu i vanjskoj kamenoj kućici s kuhinjom i roštiljem. Domaćica je super ljubazna. Definitivno preporuka i vraćamo se."
+    }
   ],
   vale: [
-    // Add Vale reviews here
+    {
+      reviewer: "Phan",
+      stars: 5,
+      text: "Diese Unterkunft hat die volle Punktzahl verdient. Ausstattung, Lage und Besitzer, alles perfekt!"
+    },
+    {
+      reviewer: "Davide",
+      stars: 5,
+      text: "Villa molto confortevole, ideale per due famiglie. Non manca nulla, il gestore è estremamente disponibile. A poca distanza da Labin, splendido borgo nel quale cenare. La zona è molto tranquilla e la piscina pulitissima."
+    },
+    {
+      reviewer: "Emanuele",
+      stars: 4,
+      text: "Anlage sehr gepflegt, Raumaufteilung war super, sauberer Swimmingpool, ruhige Lage und doch so nahe an der Stadt und nicht weit weg von Meer, sehr wichtig jedes Zimmer hat eine Klimaanlage, Gastgeber zuvorkommend. Gerne wieder."
+    },
+    {
+      reviewer: "Dawood",
+      stars: 4,
+      text: "There are no comments available for this review."
+    }
   ]
 };
 
